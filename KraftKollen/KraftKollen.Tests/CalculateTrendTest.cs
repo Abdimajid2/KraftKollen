c4ee53a0379c46611e3e23a8d01a9878ebbcf640
Archieturbo <archieturbo@live.se>
2024-12-03 11:07:13 +0100

    Created Fixtures for more readable code, more mvp, less DRY

---
+++ KraftKollen/KraftKollen.Tests/CalculateTrendTest.cs
@@ -0,0 +1,65 @@
+using KraftKollen.Helpers;
+using KraftKollen.Models;
+using Xunit;
+using System.Collections.Generic;
+
+namespace KraftKollen.Tests
+{
+    public class TrendCalculatorTests : IClassFixture<TrendCalculatorFixture>
+    {
+        private readonly TrendCalculatorFixture _fixture;
+
+        public TrendCalculatorTests(TrendCalculatorFixture fixture)
+        {
+            // Arrange 
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public void CalculateTrend_WithLessThanTwoDataPoints_ReturnsError()
+        {
+
+            // Act
+            var result = _fixture.TrendCalculator.CalculateTrend(_fixture.InsufficientTrendData);
+
+            // Assert
+            Assert.Equal("Inte tillräckligt med data för att beräkna en trend.", result);
+        }
+
+        [Fact]
+        public void CalculateTrend_WithIncreasingValues_ReturnUpwardTrend()
+        {
+
+
+            // Act
+            var result = _fixture.TrendCalculator.CalculateTrend(_fixture.IncreasingTrendData);
+
+            // Assert
+            Assert.Equal("Trenden går uppåt.", result);
+        }
+
+        [Fact]
+        public void CalculateTrend_WithDecreasingValues_ReturnDownwardTrend()
+        {
+
+
+            // Act
+            var result = _fixture.TrendCalculator.CalculateTrend(_fixture.DecreasingTrendData);
+
+            // Assert
+            Assert.Equal("Trenden går nedåt.", result);
+        }
+
+        [Fact]
+        public void CalculateTrend_WithUnchangedValues_ReturnsNoChangeTrend()
+        {
+
+
+            // Act
+            var result = _fixture.TrendCalculator.CalculateTrend(_fixture.UnchangedTrendData);
+
+            // Assert
+            Assert.Equal("Trenden är oförändrad.", result);
+        }
+    }
+}
