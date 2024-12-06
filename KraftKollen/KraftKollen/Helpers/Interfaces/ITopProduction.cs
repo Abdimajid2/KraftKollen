ecc0e567454b5477cea4cedfe39283c323adc6cf
Archieturbo <archieturbo@live.se>
2024-12-06 13:38:21 +0100

    added interface for top3productionyears

---
+++ KraftKollen/KraftKollen/Helpers/Interfaces/ITopProduction.cs
@@ -0,0 +1,9 @@
+﻿using KraftKollen.Models;
+
+namespace KraftKollen.Helpers.Interfaces
+{
+    public interface ITopProduction
+    {
+        List<(int Year, double Production)> GetTopProductionYears(List<WindPowerProduction> productionData, int topCount = 3);
+    }
+}
