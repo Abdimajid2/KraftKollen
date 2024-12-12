6d7768e0677e4b1fd29d1eb692ebbab389f975d0
Simon Lindberg <110824570+Kronan1@users.noreply.github.com>
2024-12-12 13:20:00 +0100

    Changed what is tested
    Testing if symbol element is available and element contains right symbol

--- tests/compareTwoRegions.spec.ts
+++ tests/compareTwoRegions.spec.ts
@@ -2,55 +2,7 @@ import { test, expect } from '@playwright/test';
 import { waitForDebugger } from 'inspector';
 import { cwd } from 'process';
 
-
-test('test to see if it will find the elements', async ({ page }) => {
-  
-
-  // Navigate to the page and wait for it to load completely
-  await page.goto('http://localhost:5000/CompareTwoRegionsOutputView', { waitUntil: 'load', timeout: 10000 });  // 60s timeout
-
-  // Wait for the select element to be visible
-  const yearOne = page.getByTestId('year-one')
-  await yearOne.waitFor({ state: 'visible' });
-
-  // Workaround bad practice
-  // Added timeout because the yearone option was not selected properly. Probably something with the asynchronicity
-  await page.waitForTimeout(500);
-
-  // Simulate the year selection
-  await yearOne.selectOption('2017')
-
-  // // Wait for the select elements to be visible and interactable
-  const yearTwo = page.getByTestId('year-two')
-  await yearTwo.waitFor({ state: 'visible' });
-
-  // Simulate the year selection
-  await yearTwo.selectOption('2017')
-
-  // Click the button to trigger the comparison logic
-  const button = page.getByTestId('fetch-button');
-  await button.click(); 
-
-  // Workaround bad practice
-  // More wait to let the page load the content before access the next element
-  //await page.waitForTimeout(500);
-
-  // Wait for the specific element to appear
-  //const locator = page.getByTestId('comparison-symbol');
-  //await expect(locator).toBeVisible();
-
-  // Wait for the DOM to update and display the arrow
-  // Get the computed color of the arrow element
-  const arrowElement = page.getByTestId('equal');
-  await arrowElement.waitFor({ state: 'visible' });
-
-  await expect(arrowElement).toBeVisible
-  
-});
-
-
-
-test('Ensure arrow is green when second year value is greater than first year', async ({ page }) => {
+test('Ensure right-arrow is visible when second year value is greater than first year', async ({ page }) => {
   
 
   // Navigate to the page and wait for it to load completely
@@ -78,24 +30,22 @@ test('Ensure arrow is green when second year value is greater than first year', 
   const button = page.getByTestId('fetch-button');
   await button.click(); 
 
-  // Workaround bad practice
-  // More wait to let the page load the content before access the next element
-  //await page.waitForTimeout(500);
-
-  // Wait for the specific element to appear
-  //const locator = page.getByTestId('comparison-symbol');
-  //await expect(locator).toBeVisible();
-
   // Wait for the DOM to update and display the arrow
   // Get the computed color of the arrow element
   const arrowElement = page.getByTestId('right-arrow');
   await arrowElement.waitFor({ state: 'visible' });
 
+  // Verify that element is available in dom
+  await expect(arrowElement).toBeVisible
+
+  // Verify that element contains right arrow
+  await expect(arrowElement).toHaveText('  →  ')
+
   // Verify that the color matches the expected Bootstrap green
-  await expect(arrowElement).toHaveCSS('color', 'rgb(15, 81, 50)'); // Expected green color
+  // await expect(arrowElement).toHaveCSS('color', 'rgb(15, 81, 50)'); // Expected green color
 });
 
-test('Ensure arrow is red when first year value is greater than second year', async ({ page }) => {
+test('Ensure left-arrow visible when first year value is greater than second year', async ({ page }) => {
   
 
   // Navigate to the page and wait for it to load completely
@@ -123,25 +73,23 @@ test('Ensure arrow is red when first year value is greater than second year', as
   const button = page.getByTestId('fetch-button');
   await button.click(); 
 
-  // Workaround bad practice
-  // More wait to let the page load the content before access the next element
-  //await page.waitForTimeout(500);
-
-  // Wait for the specific element to appear
-  //const locator = page.getByTestId('comparison-symbol');
-  //await expect(locator).toBeVisible();
-
   // Wait for the DOM to update and display the arrow
   // Get the computed color of the arrow element
   const arrowElement = page.getByTestId('left-arrow');
   await arrowElement.waitFor({ state: 'visible' });
+    
+  // Verify that element is available in dom
+  await expect(arrowElement).toBeVisible
+
+  // Verify that element contains left arrow
+  await expect(arrowElement).toHaveText('  ←  ')
 
   // Verify that the color matches the expected Bootstrap green
-  await expect(arrowElement).toHaveCSS('color', 'rgb(132, 32, 41)'); // Expected red color
+  // await expect(arrowElement).toHaveCSS('color', 'rgb(132, 32, 41)'); // Expected red color
 });
 
 
-test('Ensure equalsign is blue when first year value is equal to second year', async ({ page }) => {
+test('Ensure equal sign is visible when first year value is equal to second year', async ({ page }) => {
   
 
   // Navigate to the page and wait for it to load completely
@@ -169,20 +117,18 @@ test('Ensure equalsign is blue when first year value is equal to second year', a
   const button = page.getByTestId('fetch-button');
   await button.click(); 
 
-  // Workaround bad practice
-  // More wait to let the page load the content before access the next element
-  //await page.waitForTimeout(500);
-
-  // Wait for the specific element to appear
-  //const locator = page.getByTestId('comparison-symbol');
-  //await expect(locator).toBeVisible();
-
   // Wait for the DOM to update and display the arrow
   // Get the computed color of the arrow element
   const arrowElement = page.getByTestId('equal');
   await arrowElement.waitFor({ state: 'visible' });
 
+  // Verify that element is available in dom
+  await expect(arrowElement).toBeVisible
+
+  // Verify that element contains equals sign
+  await expect(arrowElement).toHaveText('  =  ')
+
   // Verify that the color matches the expected Bootstrap green
-  await expect(arrowElement).toHaveCSS('color', 'rgb(5, 81, 96)'); // Expected blue color
+  // await expect(arrowElement).toHaveCSS('color', 'rgb(5, 81, 96)'); // Expected blue color
 });
 
